0bbf24f2ff3a47404ec685695d8e8a8ab3127caf
Débora Caetano <deboravfx@gmail.com>
2026-03-24 14:56:21 +0000

    feat: intro cards component

---
+++ projeto-ipm/src/components/IntroCard.vue
@@ -0,0 +1,66 @@
+<script>
+    export default {
+        name: 'IntroCard',
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            description: {
+                type: String,
+                required: true
+            },
+            height: {
+                type: String,
+                default: '120px'
+            },
+            width: {
+                type: String,
+                default: '100%'
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="intro-card" :style="{ height: height, width: width }">
+        <div class="intro-card-content"> 
+            <h2 class="title">{{ title }}</h2>
+            <p class="description">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .intro-card{
+        background-color: var(--bg-blue);
+        padding: 16px;        
+        min-height: 120px; 
+    }
+
+    .intro-card-content{
+        display: flex;
+        flex-direction: column;
+        gap: 2px;
+    }
+
+    .description {
+        font-family: var(--font-primary);
+        font-size: 17px;
+        color: white;
+        padding-left: 25px;
+        max-width: 600px;
+        padding-top: 7px;
+    }
+
+    .title {
+        font-family: var(--font-secondary);
+        font-size: 28px;
+        font-weight: bold;
+        color: var(--bg-yellow);
+        padding-left: 25px;
+        padding-top: 130px;
+        max-width: 600px;
+    }
+
+</style>
--- projeto-ipm/src/views/InicioView.vue
+++ projeto-ipm/src/views/InicioView.vue
@@ -1,9 +1,17 @@
-<script>
+<script setup>
+import IntroCard from '@/components/IntroCard.vue';
+
+const title = "Monitorização do Mecanismo de Recuperação e Resiliência";
+const description = "Explore como cada Estado-Membro está a cumprir os seus marcos e metas para garantir um futuro resiliente.";
 </script>
 
 <template>
+  <div>
+    <IntroCard 
+      :title="title" 
+      :description="description" 
+      height="400px" 
+      width="100%" 
+    />
+  </div>
 </template>
-
-<scope>
-    
-</scope>
