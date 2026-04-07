d37749ed44d09b00d7898b9f471b56eeafdc9801
Débora Caetano <deboravfx@gmail.com>
2026-04-07 22:17:38 +0100

    feat: search-bar component (#34)

---
+++ projeto-ipm/src/components/SearchBar.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: 'Pesquisar'
+  },
+  height: {
+    type: String,
+    default: '45px' 
+  },
+  width: {
+    type: String,
+    default: '100%'
+  }
+});
+
+const searchQuery = ref('');
+defineEmits(['search']);
+
+</script>
+
+<template>
+  <div class="search-wrapper" :style="{ width: props.width }">
+    <img 
+      src="@/assets/search-icon.svg" 
+      alt="Search" 
+      class="search-icon"
+    />
+
+    <input
+      type="text"
+      v-model="searchQuery"
+      :placeholder="props.placeholder"
+      :style="{ height: props.height }"
+      @input="$emit('search', searchQuery)"
+      class="search-input"
+    />
+  </div>
+</template>
+
+<style scoped>
+.search-wrapper {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.search-icon {
+  position: absolute;
+  left: 16px; 
+  width: 16px; 
+  height: 16px;
+  pointer-events: none;
+}
+
+.search-input {
+  width: 100%;
+  padding: 0 16px 0 48px; 
+  font-size: 16px;
+  border-radius: 8px;
+
+  border: 1px solid #c8c8c8; 
+  
+  outline: none;
+  background-color: #fff;
+}
+
+.search-input::placeholder {
+  color: #9e9e9e;
+  font-size: 15px;
+  
+}
+</style>
--- projeto-ipm/src/views/PaisesView.vue
+++ projeto-ipm/src/views/PaisesView.vue
@@ -1,9 +1,26 @@
-<script>
+<script setup>
+import SearchBar from '@/components/SearchBar.vue';
+
+
 </script>
 
 <template>
+  <div class="page-content">
+    <SearchBar class="my-search" width="100%" height="36px" />
+  </div>
 </template>
 
-<scope>
-    
-</scope>
+<style scoped>
+.page-content {
+  display: flex;
+  justify-content: center; 
+  width: 100%;             
+  margin-top: 50px;   
+  padding: 20px;
+}
+
+.my-search {
+  max-width: 1200px;
+  width: 100%;
+}
+</style>
