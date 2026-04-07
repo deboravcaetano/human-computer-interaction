fa2c0c7ebc7e25f848c3bed31a28389bb1ea4028
Débora Caetano <deboravfx@gmail.com>
2026-04-07 22:17:59 +0100

    feat: top 3 cards (#30)

---
+++ projeto-ipm/src/components/Top3Cards.vue
@@ -0,0 +1,106 @@
+<script>
+export default {
+    name: 'Top3Card',
+    props: {
+        country: { type: String, required: true }, 
+        position: { type: Number, required: true },
+        progress: { type: Number, required: true },
+        projects: { type: Number, required: true },
+        total: { type: String, required: true } 
+    },
+    computed: {
+        countryImage() {
+            const fileName = this.country.toLowerCase();
+            
+            return new URL(`../assets/flags/${fileName}-flag.svg`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card-container">
+        <div class="card-header">
+            <img :src="countryImage" :alt="country" class="flag" />
+            <h2 class="country-title">{{ position }}º {{ country }}</h2>
+        </div>
+
+        <div class="card-body">
+            <div class="data-row">
+                <span class="label">Progresso do plano:</span>
+                <span class="value">{{ progress }}%</span>
+            </div>
+            <div class="data-row">
+                <span class="label">Projetos ativos:</span>
+                <span class="value">{{ projects }}</span>
+            </div>
+            <div class="data-row">
+                <span class="label">Total recebido:</span>
+                <span class="value">€{{ total }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card-container {
+    background-color: var(--bg-blue-dark);
+    border-radius: 10px;
+    padding: 20px;
+    width: 400px; 
+    height: 234px;
+    color: white;
+    font-family: sans-serif;
+    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3);
+}
+
+.card-header {
+    background-color: #283C74;
+    display: flex;
+    align-items: center;
+    width: 370px;           
+    height: 38px;          
+    
+    padding: 0 10px;      
+    border-radius: 5px;
+    margin-bottom: 40px;   
+    box-sizing: border-box; /* Garante que o padding não aumente o width/height */
+}
+
+.flag {
+    width: 67px;           
+    height: 45px;           
+    margin-right: 15px;
+    border-radius: 2px;
+}
+
+.country-title {
+    color: var(--bg-yellow); 
+    font-size: 1.3rem;     
+    margin-left: 30px;
+    font-weight: bold;
+}
+
+.card-body {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+}
+
+.data-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.label {
+    color: #0B85FF; 
+    font-weight: bold;
+}
+
+.value {
+    color: #0B85FF;
+    font-weight: bold;
+    font-size: 1rem;
+}
+</style>
--- projeto-ipm/src/views/InicioView.vue
+++ projeto-ipm/src/views/InicioView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import IntroCard from '@/components/IntroCard.vue';
 import AllFlags from '@/components/AllFlags.vue';
+import Top3Cards from '@/components/Top3Cards.vue';
+
 
 const title = "Monitorização do Mecanismo de Recuperação e Resiliência";
 const description = "Explore como cada Estado-Membro está a cumprir os seus marcos e metas para garantir um futuro resiliente.";
@@ -16,5 +18,87 @@ const description = "Explore como cada Estado-Membro está a cumprir os seus mar
     >
       <AllFlags />
     </IntroCard>
-  </div>
+    <section class="top3-section">
+          <div class="top3-container">
+            <div class="top3-header">
+              <h2 class="title-1">Top 3</h2>
+              <p class="title-2">países que lideram a execução</p>
+            </div>
+
+            <div class="top3-cards">
+              <Top3Cards
+                country="Portugal" 
+                :position="1"
+                :progress="32"
+                :projects="132"
+                total="8.18 Mil M"
+              />
+              <Top3Cards
+                country="France" 
+                :position="2"
+                :progress="32"
+                :projects="132"
+                total="8.18 Mil M"
+              />
+              <Top3Cards
+                country="Spain" 
+                :position="3"
+                :progress="32"
+                :projects="132"
+                total="8.18 Mil M"
+              />
+            </div>
+          </div>
+    </section>
+  </div> 
 </template>
+
+
+<style scoped>
+.top3-section {
+  display: flex;
+  justify-content: center; 
+  padding-top: 650px;
+  padding-bottom: 400px;
+  background-color: white; 
+}
+
+.top3-container {
+  /* Largura aproximada de 3 cards (400px cada) + gaps (40px) */
+  max-width: 1280px; 
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.top3-header {
+  margin-bottom: 20px;
+  line-height: 1.1;
+}
+
+.title-1 {
+  font-size: 3.5rem; 
+  font-weight: 800;
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+  color: #000;
+}
+
+.title-2 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+  color: #000;
+}
+
+.top3-cards {
+  display: flex;
+  flex-direction: row;
+  gap: 40px;
+  width: 100%;
+  justify-content: flex-start; 
+  flex-wrap: wrap;
+}
+</style>
